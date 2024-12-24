d46e48d5881e1043eee8fb2e7d3f7bdddaff6de1
Will Thrussell <wt@toolkitwebsites.co.uk>
2024-12-24 13:00:33 +0000

    add play button and styling

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ import GOLGrid from "./scripts/GOLGrid.js";
 const gridEl = document.querySelector(".gol-grid");
 const controllerEl = document.querySelector(".gol-controller");
 
-const grid = new GOLGrid(gridEl, 20, 20, 0, true, controllerEl);
+const grid = new GOLGrid(gridEl, 32, 32, 0, true, controllerEl);
 
 window.grid = grid;
--- scripts/GOLController.js
+++ scripts/GOLController.js
@@ -2,10 +2,37 @@ class GOLController {
   constructor(element, grid) {
     this.el = element;
     this.grid = grid;
+    this.states = {
+      playing: false,
+      intervalId: null
+    };
+
     this.init();
   }
 
   init() {
+    // Make Play button
+    const playBtn = document.createElement("a");
+    this.el.append(playBtn);
+    playBtn.classList.add("btn", "play");
+    playBtn.href = "javascript:;";
+    playBtn.innerHTML = `<i class="fa-solid fa-play"></i><i class="fa-solid fa-pause"></i>`
+
+    playBtn.addEventListener("click", () => {
+      if (this.states.playing === false) {
+        this.states.playing = true;
+        playBtn.classList.add("active")
+        this.states.intervalID = setInterval(() => {
+          this.grid?.step();
+        }, 100);
+      } else if (this.states.playing === true) {
+        this.states.playing = false;
+        playBtn.classList.remove("active");
+        clearInterval(this.states.intervalID);
+        this.states.intervalId = null;
+      }
+    });
+
     // Make Step button
     const stepBtn = document.createElement("a");
     this.el.append(stepBtn);
